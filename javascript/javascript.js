934058d4117d208b0d1465785710e650dfb9d242
Kavibharathi M <kavibharathi811@gmail.com>
2024-07-31 17:24:48 +0530

    Update javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -185,6 +185,106 @@ let arr2=[2,4];
 let c=arr1.filter((x)=>arr2.includes(x));
 console.log(c);
 
+// 18.rotating an array
+function rotateRight(arr, k) {
+    k = k % arr.length;
+    const rotatedPart = arr.slice(arr.length - k);
+    const restOfArray = arr.slice(0, arr.length - k);
+return rotatedPart.concat(restOfArray);
+}
+const array = [1, 2, 3, 4, 5];
+const rotations = 2;
+const rotatedArray = rotateRight(array, rotations);
+console.log(rotatedArray);
+
+
+// 19.inserting items at a specific position
+function insertArrayAt(arr, index, arrayToInsert) {
+    return [...arr.slice(0, index), ...arrayToInsert, ...arr.slice(index)];
+}
+const originalArray = [1, 2, 3, 7, 8, 9];
+const arrayToInsert = [4, 5, 6];
+const index = 3;
+const modifiedArray = insertArrayAt(originalArray, index, arrayToInsert);
+console.log(modifiedArray);
+
+
+// 20.comparing 2 arrays of equality
+function arraysEqual(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    return arr1.every((value, index) => value === arr2[index]);
+}
+const array1 = [1, 2, 3];
+const array2 = [1, 2, 3];
+const array3 = [1, 2, 4];
+console.log(arraysEqual(array1, array2))
+console.log(arraysEqual(array1, array3))
+
+
+// 21.creating array of arrays
+const arrayOfArrays = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+console.log(arrayOfArrays);
+
+
+// 22.array destructing
+const numbers = [1, 2, 3, 4, 5];
+const [first, second, ...rest] = numbers;
+console.log(first);
+console.log(second);
+console.log(rest);
+
+//23.checking array elements
+var fruits=["apple","orange","mango"];
+if(fruits.includes("kiwi")){
+    console.log("it includes");
+}
+else{
+    console.log("it does not include");
+}
+
+//24.array of object sorting
+let students=[
+    {dob:"11.05.2004",city:"pollachi",score:90},
+    {dob:"04.01.2003",city:"dharmapuri",score:95},
+    {dob:"10.08.2003",city:"chennai",score:60}
+];
+students.sort((a,b)=>a.score-b.score);
+
+students.forEach((e)=>{
+    console.log(e.score);
+})
+
+//25.creating a new array with given length
+function NewArray(size) {
+    var x = [];
+    for (var i = 0; i <= size; ++i) {
+        x[i] = i;
+    }
+    return x;
+}
+var d = NewArray(10);
+console.log(d);
+
+//26.finding intersection of two arrays
+let a=[1,2,3,4,5];
+let b=[3,4,5,6,7];
+let result=a.filter((element)=>b.includes(element));
+console.log(result);
+
+//27.finding the difference between two arrays
+let arr1=[1,2,3,4,5];
+let arr2=[1,2,6,7];
+let ans=arr1.filter((element)=>!arr2.includes(element));
+console.log(ans);
+
+
+
 
 
 
